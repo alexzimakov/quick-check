85efdb8c7dc9b24bcefd89931f038493595a8840
Alexander Zimakov <alexzmdev@gmail.com>
2023-01-16 13:58:37 +0700

    Update schema export

--- src/schema.ts
+++ src/schema.ts
@@ -1,7 +1,13 @@
 import { StringType, type StringTypeParams } from './string-type.js';
+import { type BooleanParams, BooleanType } from './boolean-type.js';
 
-export function string<T extends StringTypeParams>(params: T) {
-  return StringType.create<T>(params);
+export function string<T extends StringTypeParams>(params?: T) {
+  return StringType.create(params);
 }
 string.ERROR_CODES = StringType.ErrorCodes;
 string.PATTERNS = StringType.Patterns;
+
+export function boolean<T extends BooleanParams>(params?: T) {
+  return BooleanType.create(params);
+}
+boolean.ERROR_CODES = BooleanType.ErrorCodes;
