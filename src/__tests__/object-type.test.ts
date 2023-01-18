44ebf1748859948b8f173015b317286b2c1b6c85
Alexander Zimakov <alexzmdev@gmail.com>
2023-01-18 10:31:41 +0700

    Object type

---
+++ src/__tests__/object-type.test.ts
@@ -0,0 +1,331 @@
+import { describe, expect, test } from 'vitest';
+import { ObjectType } from '../type-aliases/object-type.js';
+import { StringType } from '../type-aliases/string-type.js';
+import { NumberType } from '../type-aliases/number-type.js';
+import { BooleanType } from '../type-aliases/boolean-type.js';
+import { EnumType } from '../type-aliases/enum-type.js';
+import { ArrayType } from '../type-aliases/array-type.js';
+import { RapidCheckError } from '../error.js';
+import { format } from './test-util.js';
+
+describe('positive cases', () => {
+  class TestObject {
+    str: string;
+    num: number;
+
+    constructor(str: string, num: number) {
+      this.str = str;
+      this.num = num;
+    }
+  }
+
+  const testCases = [
+    {
+      input: { str: 'foo' },
+      output: { str: 'foo' },
+    },
+    {
+      input: { str: 'foo', num: 1, unknownProp: true },
+      output: { str: 'foo', num: 1 },
+    },
+    {
+      input: new TestObject('test', 10),
+      output: { str: 'test', num: 10 },
+    },
+  ];
+  const schema = ObjectType.create({
+    str: StringType.create(),
+    num: NumberType.create().optional(),
+  });
+  testCases.forEach(({ input, output }) => {
+    test(
+      `should return ${format(input)} when value is ${format(output)}`,
+      () => expect(schema.parse(input)).toEqual(output)
+    );
+  });
+});
+
+describe('negative cases', () => {
+  const invalid = [
+    null,
+    undefined,
+    "['a', 'b', 'c']",
+    1,
+    1n,
+    [],
+    Symbol('Object'),
+  ];
+  const schema = ObjectType.create({});
+  invalid.forEach((value) => {
+    test(`should throw an error when value is ${format(value)}`, () => {
+      expect(() => schema.parse(value)).toThrow(RapidCheckError);
+    });
+  });
+});
+
+test('should allow unknown properties', () => {
+  const schema = ObjectType.create({
+    str: StringType.create(),
+    num: NumberType.create(),
+  }, { omitUnknownProps: false });
+  const value = {
+    str: 'lorem ipsum',
+    num: 10,
+    unknown: true,
+    array: [1, 2, 3],
+  };
+  expect(schema.parse(value)).toEqual(value);
+});
+
+test(
+  "throws an error when one or more props don't conform to the schema",
+  () => {
+    const schema = ObjectType.create({
+      str: StringType.create(),
+      num: NumberType.create(),
+      bool: BooleanType.create(),
+      enum: EnumType.create([1, 2, 3] as const),
+      arr: ArrayType.create(StringType.create()),
+      nested: ObjectType.create({
+        foo: StringType.create(),
+      }),
+    });
+    const value = {
+      str: 'foo',
+      num: NaN,
+      bool: false,
+      enum: -1,
+      arr: ['a', 'b', 'c', 4],
+      nested: {},
+    };
+
+    try {
+      schema.parse(value);
+    } catch (error) {
+      expect(error).toBeInstanceOf(RapidCheckError);
+
+      const errors = (error as RapidCheckError).getErrors();
+      expect(errors).toHaveLength(4);
+
+      expect(errors[0]).toBeInstanceOf(RapidCheckError);
+      expect(errors[0]).toHaveProperty('path', ['num']);
+
+      expect(errors[1]).toBeInstanceOf(RapidCheckError);
+      expect(errors[1]).toHaveProperty('path', ['enum']);
+
+      expect(errors[2]).toBeInstanceOf(RapidCheckError);
+      expect(errors[2]).toHaveProperty('path', ['arr', 3]);
+
+      expect(errors[3]).toBeInstanceOf(RapidCheckError);
+      expect(errors[3]).toHaveProperty('path', ['nested', 'foo']);
+    }
+
+    expect.assertions(10);
+  }
+);
+
+test('casts to the object a passed value', () => {
+  const schema = ObjectType.create({}, { cast: true });
+  expect(schema.parse(null)).toEqual({});
+  expect(schema.parse(undefined)).toEqual({});
+});
+
+test('throws errors with custom messages', () => {
+  const requiredError = 'is required';
+  const typeError = 'must be string';
+  const schema = ObjectType.create({}, {
+    requiredError,
+    typeError,
+  });
+  expect(() => schema.parse(null)).toThrow(requiredError);
+  expect(() => schema.parse(1)).toThrow(typeError);
+});
+
+describe('optional()', () => {
+  const schema = ObjectType.create({}).optional();
+  test('returns undefined when a passed value is undefined', () => {
+    expect(schema.parse(undefined)).toBe(undefined);
+  });
+
+  test('throws an error when a passed value is null', () => {
+    expect(() => schema.parse(null)).toThrow(RapidCheckError);
+  });
+});
+
+describe('nullable()', () => {
+  const schema = ObjectType.create({}).nullable();
+
+  test('returns null when a passed value is null', () => {
+    expect(schema.parse(null)).toBe(null);
+  });
+
+  test('throws an error when a passed value is undefined', () => {
+    expect(() => schema.parse(undefined)).toThrow(RapidCheckError);
+  });
+});
+
+describe('nullish()', () => {
+  const schema = ObjectType.create({}).nullish();
+
+  test('returns null when a passed value is null', () => {
+    expect(schema.parse(undefined)).toBe(undefined);
+  });
+
+  test('returns undefined when a passed value is undefined', () => {
+    expect(schema.parse(undefined)).toBe(undefined);
+  });
+});
+
+describe('required()', () => {
+  const optionalSchema = ObjectType.create({})
+    .optional()
+    .nullable();
+  const schema = optionalSchema.required();
+
+  test('throws an error when a passed value is undefined', () => {
+    expect(() => schema.parse(undefined)).toThrow(RapidCheckError);
+  });
+
+  test('throws an error when a passed value is null', () => {
+    expect(() => schema.parse(null)).toThrow(RapidCheckError);
+  });
+});
+
+describe('map()', () => {
+  test('returns a value from the `mapper` function', () => {
+    const schema = ObjectType.create({
+      foo: StringType.create(),
+    }).map((obj) => JSON.stringify(obj));
+    const value = { foo: 'bar' };
+    expect(schema.parse(value)).toBe(JSON.stringify(value));
+  });
+
+  test('rethrows any error from the `mapper` function', () => {
+    const error = new RapidCheckError('invalid_state', 'Invalid state.');
+    const schema = ObjectType.create({}).map(() => {
+      throw error;
+    });
+    expect(() => schema.parse({})).toThrow(error);
+  });
+});
+
+
+describe('onlyKnownProps()', () => {
+  test("returns a passed object when it doesn't have unknown props", () => {
+    const schema = ObjectType.create({
+      a: NumberType.create().optional(),
+      b: NumberType.create().optional(),
+    }).onlyKnownProps();
+    expect(schema.parse({})).toEqual({});
+    expect(schema.parse({ a: 1 })).toEqual({ a: 1 });
+    expect(schema.parse({ a: 1, b: 2 })).toEqual({ a: 1, b: 2 });
+  });
+
+  test('throws an error when the object has unknown properties', () => {
+    const schema = ObjectType.create({
+      a: NumberType.create().optional(),
+      b: NumberType.create().optional(),
+    }, { omitUnknownProps: false }).onlyKnownProps();
+    expect(() => schema.parse({
+      a: 1,
+      b: 2,
+      c: 3,
+    })).toThrow(RapidCheckError);
+  });
+
+  test('throws an error with custom error message', () => {
+    const message = 'must have only known props';
+    const obj = { a: 'foo', b: 2 };
+    expect(
+      () => ObjectType.create(
+        { a: StringType.create() },
+        { omitUnknownProps: false }
+      )
+        .onlyKnownProps({ message })
+        .parse(obj)
+    ).toThrow(message);
+    expect(
+      () => ObjectType.create(
+        { a: StringType.create() },
+        { omitUnknownProps: false }
+      )
+        .onlyKnownProps({ message: () => message })
+        .parse(obj)
+    ).toThrow(message);
+  });
+});
+
+describe('custom()', () => {
+  const propsSchema = {
+    a: NumberType.create(),
+    b: NumberType.create(),
+  };
+  const sumError = new RapidCheckError('invalid_sum', 'Invalid sum');
+  const validateSum = (value: { a: number, b: number }) => {
+    if (value.a + value.b !== 10) {
+      throw sumError;
+    }
+    return value;
+  };
+
+  test('returns a value from custom validator', () => {
+    const schema = ObjectType
+      .create(propsSchema)
+      .custom(validateSum);
+    const obj = { a: 5, b: 5 };
+    expect(schema.parse(obj)).toEqual(obj);
+  });
+
+  test(
+    "throws an error when a passed value doesn't pass custom validator",
+    () => {
+      const schema = ObjectType
+        .create(propsSchema)
+        .custom(validateSum);
+      expect(() => schema.parse({ a: 5, b: 3 })).toThrow(sumError);
+    }
+  );
+});
+
+describe('ObjectType.isObject', () => {
+  test('returns true when the value is an object', () => {
+    expect(ObjectType.isObject({})).toBe(true);
+    expect(ObjectType.isObject(Object.create(null))).toBe(true);
+    expect(ObjectType.isObject({ foo: 'bar' })).toBe(true);
+    expect(ObjectType.isObject(new Date())).toBe(true);
+  });
+
+  test('returns false when the value is not an object', () => {
+    expect(ObjectType.isObject(null)).toBe(false);
+    expect(ObjectType.isObject(undefined)).toBe(false);
+    expect(ObjectType.isObject('foo')).toBe(false);
+    expect(ObjectType.isObject(1)).toBe(false);
+    expect(ObjectType.isObject(1n)).toBe(false);
+    expect(ObjectType.isObject(true)).toBe(false);
+    expect(ObjectType.isObject([])).toBe(false);
+    expect(ObjectType.isObject(Symbol('test'))).toBe(false);
+  });
+});
+
+describe('ObjectType.formatProperties()', () => {
+  test('returns empty string when the props count is 0', () => {
+    expect(ObjectType.formatProps([])).toBe('');
+  });
+
+  test('returns first property when the props count is 1', () => {
+    expect(ObjectType.formatProps(['sum'])).toBe('sum');
+  });
+
+  test('returns props joined using " and " when the props count is 2', () => {
+    expect(ObjectType.formatProps(['a', 'b'])).toBe('a and b');
+  });
+
+  test(
+    'returns props conjunction when the props count is greater than 2',
+    () => {
+      expect(
+        ObjectType.formatProps(['a', 'b', 'c', 'd'])
+      ).toBe('a, b, c, and d');
+    }
+  );
+});
---
+++ src/type-aliases/object-type.ts
@@ -0,0 +1,279 @@
+import { type MapTypeFn } from '../types.js';
+import { TypeAlias } from './type-alias.js';
+import { RapidCheckError } from '../error.js';
+import { requiredError } from './error-messages.js';
+
+type ObjectTypeOptions = {
+  isOptional: boolean;
+  isNullable: boolean;
+  shouldCastValue: boolean;
+  shouldOmitUnknownProps: boolean;
+  typeError?: string;
+  requiredError?: string;
+};
+type PropsSchema = { [prop: string]: TypeAlias<unknown> };
+type ObjectValidator<T> = (props: T) => T;
+type ObjectValidators<T> = { [name: string]: ObjectValidator<T> };
+
+export type ObjectParams = {
+  cast?: boolean;
+  omitUnknownProps?: boolean;
+  typeError?: string;
+  requiredError?: string;
+  unknownPropsError?: string;
+};
+
+export class ObjectType<
+  Result,
+  Props extends object,
+  Cast extends boolean
+> extends TypeAlias<Result> {
+  protected readonly propsSchema: PropsSchema;
+  protected readonly options: ObjectTypeOptions;
+  protected readonly validators: ObjectValidators<Props>;
+  protected readonly mapper: MapTypeFn | undefined;
+
+  protected constructor(
+    itemSchema: PropsSchema,
+    options: ObjectTypeOptions,
+    validators: ObjectValidators<Props>,
+    mapper: MapTypeFn | undefined
+  ) {
+    super();
+    this.propsSchema = itemSchema;
+    this.options = options;
+    this.validators = validators;
+    this.mapper = mapper;
+  }
+
+  static ErrorCodes = {
+    required: 'object.required',
+    type: 'object.type',
+    invalidProps: 'object.invalidProps',
+    unknownProps: 'object.unknownProps',
+    custom: 'array.custom',
+  } as const;
+
+  static create<
+    Schema extends PropsSchema,
+    Params extends ObjectParams
+  >(propsSchema: Schema, params?: Params): ObjectType<
+    { [Prop in keyof Schema]: ReturnType<Schema[Prop]['parse']> },
+    { [Prop in keyof Schema]: ReturnType<Schema[Prop]['parse']> },
+    Params extends { cast: true } ? true : false> {
+    return new ObjectType(
+      propsSchema,
+      {
+        isOptional: false,
+        isNullable: false,
+        shouldCastValue: params?.cast ?? false,
+        shouldOmitUnknownProps: params?.omitUnknownProps ?? true,
+        requiredError: params?.requiredError,
+        typeError: params?.typeError,
+      },
+      {},
+      undefined
+    );
+  }
+
+  static isObject(value: unknown): value is Record<string, unknown> {
+    return value != null && typeof value === 'object' && !Array.isArray(value);
+  }
+
+  static formatProps(props: string[]): string {
+    if (props.length <= 2) {
+      return props.join(' and ');
+    }
+    return [...props.slice(0, -2), props.slice(-2).join(', and ')].join(', ');
+  }
+
+  optional(): ObjectType<
+    Cast extends true ? Result : Result | undefined,
+    Props,
+    Cast> {
+    return new ObjectType(
+      this.propsSchema,
+      { ...this.options, isOptional: true },
+      { ...this.validators },
+      this.mapper
+    );
+  }
+
+  nullable(): ObjectType<
+    Cast extends true ? Result : Result | null,
+    Props,
+    Cast> {
+    return new ObjectType(
+      this.propsSchema,
+      { ...this.options, isNullable: true },
+      { ...this.validators },
+      this.mapper
+    );
+  }
+
+  nullish(): ObjectType<
+    Cast extends true ? Result : Result | null | undefined,
+    Props,
+    Cast> {
+    return new ObjectType(
+      this.propsSchema,
+      { ...this.options, isOptional: true, isNullable: true },
+      { ...this.validators },
+      this.mapper
+    );
+  }
+
+  required(): ObjectType<
+    Exclude<Result, null | undefined>,
+    Props,
+    Cast> {
+    return new ObjectType(
+      this.propsSchema,
+      { ...this.options, isOptional: false, isNullable: false },
+      { ...this.validators },
+      this.mapper
+    );
+  }
+
+  map<U>(mapper: (value: Props) => U): ObjectType<U, Props, Cast> {
+    return new ObjectType(
+      this.propsSchema,
+      { ...this.options },
+      { ...this.validators },
+      mapper
+    );
+  }
+
+  parse(value: unknown): Result;
+  parse(value: unknown): unknown {
+    const { ErrorCodes } = ObjectType;
+    const { options, validators, mapper } = this;
+
+    if (options.shouldCastValue) {
+      if (value == null) {
+        value = {};
+      }
+    }
+
+    if (value == null) {
+      if (value === undefined && options.isOptional) {
+        return value;
+      }
+      if (value === null && options.isNullable) {
+        return value;
+      }
+      throw new RapidCheckError(
+        ErrorCodes.required,
+        options.requiredError || requiredError
+      );
+    }
+
+    if (!ObjectType.isObject(value)) {
+      throw new RapidCheckError(
+        ErrorCodes.type,
+        options.typeError || 'Must be an object.'
+      );
+    }
+
+    const result = options.shouldOmitUnknownProps ? {} : { ...value };
+    const propsSchema = this.propsSchema;
+    const propsError = new RapidCheckError(
+      ObjectType.ErrorCodes.invalidProps,
+      'The object has one or more invalid properties. ' +
+      'See details for more info.'
+    );
+    for (const key of Object.keys(propsSchema)) {
+      const schema = propsSchema[key];
+      const property = value[key];
+      try {
+        result[key] = schema.parse(property);
+      } catch (err) {
+        let errors: RapidCheckError[];
+        if (err instanceof RapidCheckError && err.hasErrors()) {
+          errors = err.getErrors().map(RapidCheckError.of);
+        } else {
+          errors = [RapidCheckError.of(err)];
+        }
+
+        for (const error of errors) {
+          error.path.unshift(key);
+          propsError.addError(error);
+        }
+      }
+    }
+    if (propsError.hasErrors()) {
+      throw propsError;
+    }
+
+    let props = result as Props;
+    for (const validate of Object.values(validators)) {
+      props = validate(props);
+    }
+
+    if (typeof mapper === 'function') {
+      try {
+        return mapper(props);
+      } catch (err) {
+        throw RapidCheckError.of(err);
+      }
+    }
+
+    return props;
+  }
+
+  onlyKnownProps(params?: {
+    message?: string | ((params: { unknownProps: string[] }) => string)
+  }): ObjectType<Result, Props, Cast> {
+    const code = ObjectType.ErrorCodes.unknownProps;
+    const validator: ObjectValidator<Props> = (props) => {
+      const allowedKeys: Record<string, true> = {};
+      for (const key of Object.keys(this.propsSchema)) {
+        allowedKeys[key] = true;
+      }
+
+      const unknownProps: string[] = [];
+      for (const key of Object.keys(props)) {
+        if (!allowedKeys[key]) {
+          unknownProps.push(key);
+        }
+      }
+
+      if (unknownProps.length > 0) {
+        let message: string;
+        if (params?.message) {
+          if (typeof params.message === 'function') {
+            message = params.message({ unknownProps });
+          } else {
+            message = params.message;
+          }
+        } else {
+          message = 'The object must have only known properties. ' +
+            'Unknown properties: ' +
+            ObjectType.formatProps(unknownProps);
+        }
+        throw new RapidCheckError(code, message, {
+          params: { unknownProps },
+        });
+      }
+
+      return props;
+    };
+
+    return new ObjectType(
+      this.propsSchema,
+      { ...this.options },
+      { ...this.validators, [code]: validator },
+      this.mapper
+    );
+  }
+
+  custom(validator: ObjectValidator<Props>): ObjectType<Result, Props, Cast> {
+    const code = ObjectType.ErrorCodes.custom;
+    return new ObjectType(
+      this.propsSchema,
+      { ...this.options },
+      { ...this.validators, [code]: validator },
+      this.mapper
+    );
+  }
+}
