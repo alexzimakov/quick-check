c52182248df9c2429b1096a7945ed14d0335085b
Alexander Zimakov <alexzmdev@gmail.com>
2023-01-19 16:56:19 +0700

    Fix build script

---
+++ rollup.config.js
@@ -0,0 +1,66 @@
+import path from 'node:path';
+import del from 'rollup-plugin-delete';
+import typescript from '@rollup/plugin-typescript';
+
+const input = './src/index.ts';
+const outputDir = {
+  types: './types',
+  esm: './esm',
+  cjs: './cjs',
+};
+export default [
+  // Types
+  {
+    input,
+    output: {
+      dir: outputDir.types,
+      format: 'es',
+    },
+    plugins: [
+      del({ targets: outputDir.types }),
+      del({
+        targets: path.join(outputDir.types, '/index.js'),
+        hook: 'writeBundle',
+      }),
+      typescript({
+        removeComments: false,
+        declaration: true,
+        declarationDir: outputDir.types,
+      }),
+    ],
+  },
+
+  // ES modules
+  {
+    input,
+    output: {
+      dir: outputDir.esm,
+      format: 'es',
+      preserveModules: true,
+    },
+    plugins: [
+      del({ targets: outputDir.esm }),
+      typescript(),
+    ],
+  },
+
+  // CommonJS modules
+  {
+    input,
+    output: {
+      dir: outputDir.cjs,
+      format: 'cjs',
+      exports: 'named',
+      preserveModules: true,
+      entryFileNames: '[name].cjs',
+      generatedCode: {
+        arrowFunctions: true,
+        constBindings: true,
+      },
+    },
+    plugins: [
+      del({ targets: outputDir.cjs }),
+      typescript(),
+    ],
+  },
+];
--- scripts/rm.js
+++
@@ -1,6 +0,0 @@
-import { rm } from 'node:fs/promises';
-
-const [, , dirPath] = process.argv;
-(async () => {
-  await rm(dirPath, { recursive: true, force: true });
-})();
