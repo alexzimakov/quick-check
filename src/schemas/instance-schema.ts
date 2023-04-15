0a527d6503b8832660c2247f7515b13278748415
Alexander Zimakov <alexzmdev@gmail.com>
2023-04-15 12:14:22 +0700

    Instance schema

---
+++ src/schemas/instance-schema.ts
@@ -0,0 +1,45 @@
+import { RequiredErrorMessage, Rule, Schema, TypeErrorMessage } from './schema.js';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Constructor<T> = new(...args: any[]) => T;
+
+export class InstanceSchema<T> extends Schema<T> {
+  protected readonly _constructor: Constructor<T>;
+
+  constructor(
+    constructor: Constructor<T>,
+    rules?: Rule<T>[],
+    typeError?: TypeErrorMessage,
+    requiredError?: RequiredErrorMessage,
+  ) {
+    super(rules, typeError, requiredError);
+    this._constructor = constructor;
+  }
+
+  protected _validate(value: unknown): T {
+    const constructor = this._constructor;
+    if (!(value instanceof constructor)) {
+      const expectedType = constructor.name;
+      this._throwTypeError(value, expectedType);
+    }
+    return value;
+  }
+}
+
+export type InstanceSchemaOptions<T> = {
+  rules?: Rule<T>[];
+  typeError?: TypeErrorMessage;
+  requiredError?: RequiredErrorMessage;
+};
+
+export function createInstanceSchema<T>(
+  constructor: Constructor<T>,
+  options?: InstanceSchemaOptions<T>,
+) {
+  return new InstanceSchema<T>(
+    constructor,
+    options?.rules,
+    options?.typeError,
+    options?.requiredError,
+  );
+}
