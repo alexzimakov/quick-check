9f4549dcb6574bb2186b1e9b8f3e0d1d33f3d2ef
Alexander Zimakov <alexzmdev@gmail.com>
2023-04-15 12:08:38 +0700

    Boolean schema

---
+++ src/schemas/boolean-schema.ts
@@ -0,0 +1,60 @@
+import { RequiredErrorMessage, Rule, Schema, TypeErrorMessage } from './schema.js';
+
+type BooleanRule = Rule<boolean>;
+
+export class BooleanSchema extends Schema<boolean> {
+  readonly shouldCoerceType: boolean;
+
+  constructor(
+    shouldCoerceType?: boolean,
+    rules?: BooleanRule[],
+    typeError?: TypeErrorMessage,
+    requiredError?: RequiredErrorMessage,
+  ) {
+    super(rules, typeError, requiredError);
+    this.shouldCoerceType = shouldCoerceType || false;
+  }
+
+  protected override _prepareValue(value: unknown): unknown {
+    if (this.shouldCoerceType) {
+      if (
+        value === 'true' ||
+        value === 1
+      ) {
+        value = true;
+      } else if (
+        value === 'false' ||
+        value === 0 ||
+        value === null ||
+        value === undefined
+      ) {
+        value = false;
+      }
+    }
+
+    return value;
+  }
+
+  protected _validate(value: unknown): boolean {
+    if (typeof value !== 'boolean') {
+      this._throwTypeError(value, 'boolean');
+    }
+    return value;
+  }
+}
+
+export type BooleanSchemaOptions = {
+  coerceType?: boolean;
+  rules?: BooleanRule[];
+  typeError?: TypeErrorMessage;
+  requiredError?: RequiredErrorMessage;
+};
+
+export function createBooleanSchema(options?: BooleanSchemaOptions) {
+  return new BooleanSchema(
+    options?.coerceType,
+    options?.rules,
+    options?.typeError,
+    options?.requiredError,
+  );
+}
